682a2391370b4990be6513623cc971d2681100a2
Xavier Romero <o.romero.lopez@gmail.com>
2016-05-13 20:02:12 -0700

    se anadio paginadores al proyecto

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,5 +1,13 @@
 angular.module('appTareas')
+
     .controller('ctrlAlta', function($scope, $state, comun) {
+        $scope.currentPage = 0;
+        $scope.pageSize = 5;
+        $scope.pages = [];
+        $scope.users = [];
+        $scope.actuales = [];
+        $scope.actual = {};
+
     $scope.tarea = {}
     // $scope.tareas = [];
     comun.getAll();
@@ -33,6 +41,61 @@ angular.module('appTareas')
         comun.tarea = tarea;
         $state.go('editar');
     }
+        /***inicio recorset  *****/
+        $scope.consultarTodos = function()
+        {
+                $scope.users = comun.tareas;
+
+                $scope.pages.length = 0;
+                var ini = $scope.currentPage - 1;
+                var fin = $scope.currentPage + 1;
+                if(ini < 1)
+                {
+                    ini = 1;
+                    if(Math.ceil($scope.users.length / $scope.pageSize) > 5)
+                    {
+                        fin = 5;
+                    }
+                    else
+                    {
+                        fin = Math.ceil($scope.users.length / $scope.pageSize);
+                    }
+                }
+                else
+                {
+                    if (ini >= Math.ceil($scope.users.length / $scope.pageSize) - 5)
+                    {
+                        ini =  Math.ceil($scope.users.length / $scope.pageSize) - 5;
+                        fin =  Math.ceil($scope.users.length / $scope.pageSize);
+                    }
+                }
+
+                if (ini < 1)
+                {
+                    ini = 1;
+                }
+
+                for (var i = ini; i <= fin; i++)
+                {
+                    $scope.pages.push({
+                        no: i
+                    });
+                }
+
+                if ($scope.currentPage >= $scope.pages.length)
+                {
+                    $scope.currentPage = $scope.pages.length - 1;
+                }
+
+                $scope.currentPage = 0;
+
+        }
+        $scope.setPage = function(index)
+        {
+            $scope.currentPage = index - 1;
+        }
+
+
 
 })
     .controller('ctrlEditar', function($scope, $state, comun) {
@@ -47,4 +110,11 @@ angular.module('appTareas')
             comun.delete($scope.tarea);
             $state.go('alta');
         }
-    });
+    }).filter('startFromGrid', function()
+{
+    return function(input, start)
+    {
+        start =+ start;
+        return input.slice(start)
+    }
+});
